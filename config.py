4af6d71aad50d0b5100491fab7d14f5d181e0181
deafmsn201111-sys <deafmsn201111@gmail.com>
2026-06-26 01:32:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 TELEGRAM_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID    = os.environ.get("TG_CHAT_ID", "")
-MIN_LIQ_USD = float(os.environ.get("MIN_LIQ_USD", "10000"))
+MIN_LIQ_USD = float(os.environ.get("MIN_LIQ_USD", "500"))
 
 BINANCE_WS_URL = "wss://fstream.binance.com/ws/NEARUSDT@forceOrder"
 BINANCE_SYMBOL = "NEARUSDT"
@@ -14,4 +14,4 @@ HYPERLIQUID_WS_URL = "wss://api.hyperliquid.xyz/ws"
 HYPERLIQUID_COIN   = "NEAR"
 
 MIN_MSG_INTERVAL = 3
-HTTP_PORT = int(os.environ.get("PORT", 10000))
+HTTP_PORT = int(os.environ.get("PORT", 500))
