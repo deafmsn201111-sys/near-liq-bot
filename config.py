bece62ca30779a4df8dc9466864540d381c4c465
deafmsn201111-sys <deafmsn201111@gmail.com>
2026-06-26 14:56:42 +0500

    Update config.py
    Fix bugs

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ import os
 
 TELEGRAM_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID    = os.environ.get("TG_CHAT_ID", "")
+
 MIN_LIQ_USD = float(os.environ.get("MIN_LIQ_USD", "1000"))
 
 BINANCE_SYMBOLS = ["NEARUSDT", "BTCUSDT"]
-BYBIT_SYMBOLS   = ["NEARUSDT", "BTCUSDT"]
+BYBIT_SYMBOLS = ["NEARUSDT", "BTCUSDT"]
 HYPERLIQUID_COINS = ["NEAR", "BTC"]
 
 MIN_MSG_INTERVAL = 3
